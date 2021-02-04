b693b0d57d326505ff402377e29df49b5ae066ff
Андрей Савицкий <Lessthree@mail.ru>
2021-02-04 14:20:27 +0300

    TranslateButton handler added

--- Phoneword/ViewController.cs
+++ Phoneword/ViewController.cs
@@ -14,6 +14,28 @@ namespace Phoneword
         {
             base.ViewDidLoad();
             // Perform any additional setup after loading the view, typically from a nib.
+
+            string translatedNumber = String.Empty;
+
+            TranslateButton.TouchUpInside += (object sender, EventArgs e) => {
+                // Convert the phone number with text to a number
+                // using PhoneTranslator.cs
+                translatedNumber = PhoneTranslator.ToNumber(PhoneNumberText.Text);
+
+                // Dismiss the keyboard if text field was tapped
+                PhoneNumberText.ResignFirstResponder();
+
+                if (translatedNumber == String.Empty)
+                {
+                    CallButton.SetTitle("Call ", UIControlState.Normal);
+                    CallButton.Enabled = false;
+                }
+                else
+                {
+                    CallButton.SetTitle("Call " + translatedNumber, UIControlState.Normal);
+                    CallButton.Enabled = true;
+                }
+            };
         }
 
         public override void DidReceiveMemoryWarning()
